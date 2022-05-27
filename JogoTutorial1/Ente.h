85c70063727a15da6252d146953a5ad69d5f3843
LeonardoCardozo616 <leon52.cpere@outlook.com>
2022-05-27 10:11:03 -0300

    Making new modifications in the class "Ente.h" and "Ente.cpp", based on Mateus Borda Project.

--- JogoTutorial1/Ente.h
+++ JogoTutorial1/Ente.h
@@ -7,8 +7,15 @@ protected:
 	CoordF posicao;
 	CoordF tamanho;
 public:
-	Ente();
+	Ente(CoordF posicao = CoordF(0.0f, 0.0f) ,	CoordF tamanho = CoordF(0.0f, 0.0f));
 	~Ente();
+	void setPosicao(CoordF pos);
+	CoordF getPosicao() const;
+	void setTamanho(CoordF tam);
+	CoordF getTamanho() const;
+	//virtual void render() = 0;
+	//virtual void update(const float dt) = 0;
+	//virtual void inicializa() = 0;
 	void executar() {};
 	void imprimir();
 };
