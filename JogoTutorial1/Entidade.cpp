5bee0852fdc6f029c5a66e2be509d84bbc4b24c1
LeonardoCardozo616 <leon52.cpere@outlook.com>
2022-05-27 22:37:36 -0300

    New modifications on "Entidade.h" and "Entidade.cpp"

--- JogoTutorial1/Entidade.cpp
+++ JogoTutorial1/Entidade.cpp
@@ -1,12 +1,19 @@
 #include "Entidade.h"
 
-Entidade::Entidade():
-body(sf::Vector2f(100.f, 100.f))
+Entidade::Entidade(CoordF posicao, CoordF tamanho, ID id):
+	Ente(posicao, tamanho),
+	id(id),
+	body(sf::Vector2f(100.f, 100.f))
 {
 }
 
 Entidade::~Entidade()
 {
+}
+
+ID Entidade::getID() const
+{
+	return id;
 }
 
 sf::RectangleShape* Entidade::getBody()
--- JogoTutorial1/Entidade.h
+++ JogoTutorial1/Entidade.h
@@ -1,16 +1,35 @@
 #pragma once
 #include<SFML/Graphics.hpp>
 #include"Ente.h"
+#include<math.h>
+
+enum  ID {
+	vazio = 0,
+	jogador,
+	obst_A,
+	obst_B,
+	obst_C,
+	inimigo_A,
+	inimigo_B,
+	inimigo_C
+};
+
 class Entidade:
 	public Ente
 {
 protected:
+	ID id;
 	sf::RectangleShape body;
 	//sf::RenderWindow *window;
 
 public:
-	Entidade(); 
+	Entidade(CoordF posicao = CoordF(0.0f, 0.0f), CoordF tamanho = CoordF(0.0f, 0.0f), ID id = vazio); 
 	~Entidade();
+	ID getID() const;
+	//virtual void render() = 0;
+	//virtual void update(const float dt) = 0;
+	//virtual void inicializa() = 0;
+	//virtual void colisao(Entidade outraEntidade, CoordF intereccao) = 0;
 
 	sf::RectangleShape* getBody();
 	//void setWindow(sf::RenderWindow* w) { this->window = w; }
